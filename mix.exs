17db36480b0a5256277019407d9ce9ff26c08c59
MATSUMOTO Eiji <e.mattsan@gmail.com>
2025-04-09 21:22:39 +0900

    Squashed commit of the following:
    commit 51390dbe6347756d91171aef87547f0e68f8f7e4
    Author: MATSUMOTO Eiji <e.mattsan@gmail.com>
    Date:   Wed Apr 9 21:22:24 2025 +0900
    
        remove redundant comments
    
    commit e79fe766ee2e89f4f30ae08edc741408636da253
    Author: MATSUMOTO Eiji <e.mattsan@gmail.com>
    Date:   Wed Apr 9 21:21:33 2025 +0900
    
        add HTTP client package
    
    commit 758c5816cc7f7fb0d7ab43064815d45232ad69f5
    Author: MATSUMOTO Eiji <e.mattsan@gmail.com>
    Date:   Wed Apr 9 21:20:01 2025 +0900
    
        add dev packages

--- mix.exs
+++ mix.exs
@@ -12,19 +12,19 @@ defmodule DiscordWebhook.MixProject do
     ]
   end
 
-  # Run "mix help compile.app" to learn about applications.
   def application do
     [
       extra_applications: [:logger]
     ]
   end
 
-  # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
+      {:req, "~> 0.5"},
+      {:dialyxir, "~> 1.0", only: [:dev]},
+      {:credo, "~> 1.0", only: [:dev]},
+      {:ex_doc, "~> 0.37", only: [:dev]},
       {:igniter, "~> 0.5", only: [:dev, :test]}
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
   end
 end
